9c2de7ac62c35e81685d77af415bbbc575d2d606
cleiver <cleivereng@gmail.com>
2023-10-28 17:59:36 -0300

    hashtable using util

--- src/com/cleiver/aula/java/Arrays.java
+++ src/com/cleiver/aula/java/Arrays.java
@@ -1,9 +1,18 @@
 package com.cleiver.aula.java;
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Arrays {
 
 	public static void main(String[] args) {
+		Hashtable<String, Number> hashTable = new Hashtable<>();
+		hashTable.put("one", 1);
+		hashTable.put("two", 2);
+		hashTable.put("three", 3);
+		hashTable.put("four", 4);
+		System.out.printf("the value of key called 'one' is: %d\n", hashTable.get("one"));
+		
+		
 		ArrayList<Integer> intArray = new ArrayList<>();
 		for(int i = 0; i < 10; i++) {
 			intArray.add(i);
---
+++ src/com/cleiver/aula/java/HashMapJava.java
@@ -0,0 +1,11 @@
+package com.cleiver.aula.java;
+import java.util.Map;
+import java.util.HashMap;
+
+public class HashMapJava {
+
+	public static void main(String[] args) {
+		Map<>
+	}
+
+}
