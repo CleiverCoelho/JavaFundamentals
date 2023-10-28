2002451609b7cae55aa4e9b07a0274ec77d54489
cleiver <cleivereng@gmail.com>
2023-10-28 17:53:04 -0300

    listas em java com util arraylist

---
+++ src/com/cleiver/aula/java/Arrays.java
@@ -0,0 +1,15 @@
+package com.cleiver.aula.java;
+import java.util.ArrayList;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> intArray = new ArrayList<>();
+		for(int i = 0; i < 10; i++) {
+			intArray.add(i);
+		}
+		
+		System.out.print(intArray);
+	}
+
+}
